ff469ec27553dade9b8d60430ba8672a6712d7ee
inf <inf@MacBook-Pro-de-inf.local>
2018-11-15 17:27:34 +0000

    Init Commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,33 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = ObjectID();
+console.log(obj);
+
+MongoClient.connect(
+    'mongodb://localhost:27017/TodoApp', 
+    { useNewUrlParser: true }, 
+    (err, client) => {
+        if (err) {
+            return console.log('Unable to connect to MongoDB server');
+        }
+        console.log('Connected to MongoDB server.');
+        const db = client.db('TodoApp');
+
+        // db.collection('Todos').find({_id: new ObjectID('5bea075d9f25538e756884ae')}).toArray().then((docs) => {
+        //     console.log('Todos');
+        //     console.log(JSON.stringify(docs, undefined, 2));
+        // }, (err) => {
+        //     console.log('Unable to fetch todos', err);
+        // });
+
+        db.collection('Todos').find().count().then((docs) => {
+            console.log('Todos Count:', docs);
+        }, (err) => {
+            console.log('Unable to fetch todos', err);
+        });        
+
+
+        //client.close();
+    }
+);
