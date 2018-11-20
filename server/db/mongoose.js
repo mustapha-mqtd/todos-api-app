69093bc39efdc27723052361f8c9524fcb80e01e
inf <inf@MacBook-Pro-de-inf.local>
2018-11-20 16:49:11 +0000

    commit it

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 mongoose.connect(
     process.env.MONGODB_URI,
-    { useNewUrlParser: true }
+    { useNewUrlParser: true, useCreateIndex: true }
 );
 mongoose.set('debug', false);
 mongoose.set('useFindAndModify', false);
